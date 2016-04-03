4f3ee77152c0287ecb41376afc17143833adf4bd
Rogerio Muniz <rogerio@ciatec.net>
2016-04-03 19:13:36 -0300

    adicionados arquivos genericos caixa

---
+++ src/resources/generico/remessa/cnab240/Generico0.php
@@ -0,0 +1,18 @@
+<?php
+namespace CnabPHP\resources\generico\remessa\cnab400;
+use CnabPHP\RegistroAbstract;
+
+class Generico0 extends RegistroAbstract
+{
+	protected $meta = array(
+		'tipo_de_registro'=>
+		array('posicao'=>array('i'=>1,'f'=>1),'tamanho'=>3,'default'=>'0','tipo'=>'numero','required'=>true),
+		'operacao'=>
+		array('posicao'=>array('i'=>2,'f'=>2),'tamanho'=>1,'default'=>'1','tipo'=>'numero','required'=>true)
+		'data'=>
+		array('posicao'=>array('i'=>2,'f'=>2),'tamanho'=>6,'default'=>'1','tipo'=>'numero','required'=>true)
+		// definir todos os campos do registro nesse formato
+	);
+}
+
+?>
