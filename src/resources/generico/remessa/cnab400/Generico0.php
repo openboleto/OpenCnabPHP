d542f14e5e3325f73098424465959a7ad2fb4c56
Rogerio Muniz <rogerio@ciatec.net>
2016-04-04 14:17:53 -0300

    exclusão da variável $data nos arquivos genéricos
    o layout será escrito, pelo menos para cnb240 caixa nos arquivos

--- src/resources/generico/remessa/cnab400/Generico0.php
+++ src/resources/generico/remessa/cnab400/Generico0.php
@@ -4,13 +4,6 @@ use CnabPHP\RegistroAbstract;
 
 class Generico0 extends RegistroAbstract
 {
-	protected $meta = array(
-		'tipo_de_registro'=>
-		array('posicao'=>array('i'=>1,'f'=>1),'tamanho'=>3,'default'=>'0','tipo'=>'numero','required'=>true),
-		'operacao'=>
-		array('posicao'=>array('i'=>2,'f'=>2),'tamanho'=>1,'default'=>'1','tipo'=>'numero','required'=>true)
-		// definir todos os campos do registro nesse formato
-	);
 }
 
 ?>
--- src/resources/generico/remessa/cnab400/Generico5.php
+++ src/resources/generico/remessa/cnab400/Generico5.php
@@ -4,13 +4,6 @@ use rctnet\RegistroAbstract;
 
 class Generico5 extends RegistroAbstract
 {
-	protected $meta = array(
-		'tipo_de_registro'=>
-		array('posicao'=>array('i'=>1,'f'=>1),'tamanho'=>1,'default'=>'0','tipo'=>'numero'),
-		'operacao'=>
-		array('posicao'=>array('i'=>2,'f'=>2),'tamanho'=>1,'default'=>'1','tipo'=>'numero')
-		// definir todos os campos do registro nesse formato
-	);
 }
 
 ?>
