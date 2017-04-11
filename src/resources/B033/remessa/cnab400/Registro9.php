bbe3848c914229f0efd5cc4a1ce39d79e9106e03
kayna guarany <kayna@guarany.net>
2017-04-11 10:43:33 -0300

    Adiciona metodos para preenchimento de campos do registro9

--- src/resources/B033/remessa/cnab400/Registro9.php
+++ src/resources/B033/remessa/cnab400/Registro9.php
@@ -2,6 +2,7 @@
 namespace CnabPHP\resources\b033\remessa\cnab400;
 
 use CnabPHP\resources\generico\remessa\cnab400\Generico9;
+use cnabPHP\RemessaAbstract;
 
 class Registro9 extends Generico9
 {
@@ -33,6 +34,37 @@ class Registro9 extends Generico9
             'tipo'=>'int',
             'required'=>true),
    	);
+
+
+    protected function set_qtde_documento($value)
+    {
+        $registros = $this->getRegistros();
+
+        $this->data['qtde_documento'] = count($registros);
+    }
+
+    protected function set_valor_total_titulo()
+    {
+        $this->data['valor_total_titulo'] = $this->getTotal();
+    }
+
+    protected function getTotal()
+    {
+        return array_reduce($this->getRegistros(), function($soma, $registro) {
+            return $soma + $registro->getValor();
+        });
+    }
+
+    protected function getRegistros()
+    {
+        $lotes = RemessaAbstract::getLotes();
+        
+        $registros = array_filter($lotes, function($lote) {
+            return $lote instanceof Registro1;
+        });
+
+        return $registros;
+    }
 
 }
 
