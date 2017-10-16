4a44271f5868b35502f9917da96726641d1be239
Breno Douglas Araujo Souza <breno_douglas@hotmail.com>
2017-10-16 16:57:58 -0200

    Fix UTF-8 message

--- src/resources/generico/remessa/cnab240/Generico1.php
+++ src/resources/generico/remessa/cnab240/Generico1.php
@@ -56,7 +56,7 @@ class Generico1 extends RegistroRemAbstract
         {
             $this->data['tipo_inscricao'] =  $value;
         }else{
-            throw new Exception("O tipo de incri��o deve ser 1  para CPF e 2 para CNPJ, o valor informado foi:".$value);       
+            throw new Exception("O tipo de inscrição deve ser 1  para CPF e 2 para CNPJ, o valor informado foi:".$value);       
         }
     }
 
