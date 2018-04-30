936d36675882c815532a49cbfe90d3b6dc8a5b42
Gildonei Mendes Anacleto Junior <gildonei.mendes@gmail.com>
2018-04-30 16:23:54 -0300

    Adicionado método set_nosso_numero
    Adicionados os métodos set_nosso_numero e modulo11 para calcular o DV do nosso número exigido pelo santander

--- src/resources/B033/remessa/cnab240/Registro3P.php
+++ src/resources/B033/remessa/cnab240/Registro3P.php
@@ -345,6 +345,15 @@ class Registro3P extends Generico3
     );
 
     /**
+     * Sobrescreve a informação do Nosso Número incluindo o DV ao final
+     * @param int $value
+     */
+    protected function set_nosso_numero($value)
+    {
+        $this->data['nosso_numero'] = $value . self::modulo11($value);
+    }
+
+    /**
      * Sobrescreve a informação do Beneficiário com os dados da Conta, orientações Santander
      * @param int $value
      */
@@ -379,6 +388,46 @@ class Registro3P extends Generico3
             isset($data['informacao_pagador'])) {
             $class = 'CnabPHP\resources\\B' . RemessaAbstract::$banco . '\remessa\\' . RemessaAbstract::$layout . '\Registro3R';
             $this->children[] = new $class($data);
+        }
+    }
+
+    /**
+     * Cálculo do módulo 11
+     * @param int $index
+     * @return int
+     */
+    protected static function modulo11($num, $base=9, $r=0)
+    {
+
+        $soma = 0;
+        $fator = 2;
+
+        // Separacao dos numeros
+        for ($i = strlen($num); $i > 0; $i--) {
+            // pega cada numero isoladamente
+            $numeros[$i] = substr($num,$i-1,1);
+            // Efetua multiplicacao do numero pelo falor
+            $parcial[$i] = $numeros[$i] * $fator;
+            // Soma dos digitos
+            $soma += $parcial[$i];
+            if ($fator == $base) {
+                // restaura fator de multiplicacao para 2
+                $fator = 1;
+            }
+            $fator++;
+        }
+
+        // Calculo do modulo 11
+        if ($r == 0) {
+            $soma *= 10;
+            $digito = $soma % 11;
+            if ($digito == 10) {
+                $digito = 0;
+            }
+            return $digito;
+        } elseif ($r == 1){
+            $resto = $soma % 11;
+            return $resto;
         }
     }
 }
