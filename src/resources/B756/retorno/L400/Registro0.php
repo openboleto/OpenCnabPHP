31c8e5dc876878225569e0b8aa3728c79f878aac
Breno Douglas <breninho94@gmail.com>
2018-11-08 17:59:21 -0200

    Fix namespaces

--- src/resources/B756/retorno/L400/Registro0.php
+++ src/resources/B756/retorno/L400/Registro0.php
@@ -23,7 +23,7 @@
 * OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
 * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 */
-namespace CnabPHP\resources\b756\retorno\L400; 
+namespace CnabPHP\resources\B756\retorno\L400; 
 use CnabPHP\resources\generico\retorno\L400\Generico0;
 use CnabPHP\RetornoAbstract;
 
