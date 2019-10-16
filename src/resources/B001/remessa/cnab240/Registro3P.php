b5c272b668cc941ece64397c86a12c60c1e6394f
Vinícius Oliveira <viniciuswp11@gmail.com>
2019-10-16 10:50:15 -0300

    Adicionando algoritmo 'modulo11' na classe 'RegistroP' do banco 'B001' para gerar o digito verificador para convenios com 4 ou 6 digitos.

--- src/resources/B001/remessa/cnab240/Registro3P.php
+++ src/resources/B001/remessa/cnab240/Registro3P.php
@@ -316,6 +316,7 @@ class Registro3P extends Generico3 {
             case 4:
                 //$numero =$convenio. str_pad($sequencial, 7, '0', STR_PAD_LEFT);
                 $numero =$convenio.substr($sequencial,0,7);
+                $numero .= self::modulo11($numero);
 			
                 break;
             // Convênio de 6 dígitos, são 11 dígitos no nosso número
@@ -324,10 +325,12 @@ class Registro3P extends Generico3 {
                 if ($carteira== 21) {
                 $numero=$sequencial;
                 } else {
-					 
-                    $numero = $convenio .substr($sequencial,0,5);
+                    $numero = $convenio . substr($sequencial,0,5);
 					
                 }
+
+                $numero .= self::modulo11($numero);
+
                 break;
 
             // Convênio de 7 dígitos, são 17 dígitos no nosso número
@@ -343,7 +346,45 @@ class Registro3P extends Generico3 {
         }
 //$numero=str_pad($numero, 20, ' ');// inserindo zeros a direifta para completar 20 de espaço
 		//echo"numero: $numero <br>";
-  $this->data['nosso_numero'] =$numero;
+  $this->data['nosso_numero'] = $numero;
+    }
+    /**
+     * Cálculo do módulo 11
+     * @param int $index
+     * @return int
+     */
+    protected static function modulo11($num, $base=9, $r=0)
+    {
+        $soma = 0;
+        $fator = 2;
+
+        // Separacao dos numeros
+        for ($i = strlen($num); $i > 0; $i--) {
+            // pega cada numero isoladamente
+            $numeros[$i] = substr($num,$i-1,1);
+            // Efetua multiplicacao do numero pelo falor
+            $parcial[$i] = $numeros[$i] * $fator;
+            // Soma dos digitos
+            $soma += $parcial[$i];
+            if ($fator == $base) {
+                // restaura fator de multiplicacao para 2
+                $fator = 1;
+            }
+            $fator++;
+        }
+
+        // Calculo do modulo 11
+        if ($r == 0) {
+            $soma *= 10;
+            $digito = $soma % 11;
+            if ($digito == 10) {
+                $digito = 0;
+            }
+            return $digito;
+        } elseif ($r == 1){
+            $resto = $soma % 11;
+            return $resto;
+        }
     }	
 	
 }
