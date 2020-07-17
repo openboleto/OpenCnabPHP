571a36498b2305185857009cb6bec013bceb2150
Rogerio Muniz <rogerio@quilhasoft.com.br>
2020-07-17 09:14:54 -0300

    correção para layouts em caixa alta

--- src/resources/B033/remessa/cnab240/Registro3P.php
+++ src/resources/B033/remessa/cnab240/Registro3P.php
@@ -377,13 +377,13 @@ class Registro3P extends Generico3 {
 
     public function inserirDetalhe($data) {
         if ((int) $data['codigo_movimento'] != 2) {
-            $class = 'CnabPHP\resources\\B' . RemessaAbstract::$banco . '\remessa\\' . strtolower(RemessaAbstract::$layout) . '\Registro3Q';
+            $class = 'CnabPHP\resources\\B' . RemessaAbstract::$banco . '\remessa\\' . RemessaAbstract::$layout . '\Registro3Q';
             $this->children[] = new $class($data);
             if (isset($data['codigo_desconto2']) ||
                     isset($data['codigo_desconto3']) ||
                     isset($data['vlr_multa']) ||
                     isset($data['informacao_pagador'])) {
-                $class = 'CnabPHP\resources\\B' . RemessaAbstract::$banco . '\remessa\\' . strtolower(RemessaAbstract::$layout) . '\Registro3R';
+                $class = 'CnabPHP\resources\\B' . RemessaAbstract::$banco . '\remessa\\' . RemessaAbstract::$layout . '\Registro3R';
                 $this->children[] = new $class($data);
             }
         }
