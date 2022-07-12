bf98756e5bc0ba7fc97e61d7021ee6bc61fe2a4c
Rogerjeto <rogerio@quilhasoft.com.br>
2022-07-12 14:31:02 -0300

    ajuste psr4

--- src/resources/B001/retorno/L030/Registro3S3.php
+++ src/resources/B001/retorno/L030/Registro3S3.php
@@ -23,7 +23,7 @@
  * OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
  * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
-namespace CnabPHP\resources\B104\retorno\L030;
+namespace CnabPHP\resources\B001\retorno\L030;
 use CnabPHP\resources\generico\remessa\cnab240\Generico3;
 use CnabPHP\Exception;
 
