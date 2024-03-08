41d29953d8d1d9ba7884d679e68662618439ba68
brenoriot <brenopsilva95@hotmail.com>
2024-03-08 14:53:00 -0300

    correcao namespace novo arquivo

--- src/resources/B104/retorno/L101/Registro0.php
+++ src/resources/B104/retorno/L101/Registro0.php
@@ -23,7 +23,7 @@
  * OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
  * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
-namespace CnabPHP\resources\B104\retorno\L030; // SIGCB
+namespace CnabPHP\resources\B104\retorno\L101; // SIGCB
 use CnabPHP\resources\generico\retorno\L030\Generico0;
 use CnabPHP\RetornoAbstract;
 
