8c6740bed3b5e700adf1d862697532a5b14f2f1a
Breno Ryoti <brenopsilva95@hotmail.com>
2024-11-06 10:02:05 -0300

    lendo dados pix no retorno

--- samples/ExemploRetornoSantander.php
+++ samples/ExemploRetornoSantander.php
@@ -26,27 +26,26 @@
 namespace CnabPHP\samples;
 use \CnabPHP\Retorno;
 include("../autoloader.php");
-$fileContent = file_get_contents("COBST_QIAC_02_010617P_MOV.TXT");
+$fileContent = file_get_contents("RetornoSantanderPix.RET");
 
 $arquivo = new Retorno($fileContent);
 
 $registros = $arquivo->getRegistros();
 foreach($registros as $registro)
 {
-	if($registro->codigo_movimento==2){
+    if($registro->codigo_movimento==2){
 		$nossoNumero   = $registro->nosso_numero;
 		$valorRecebido = $registro->vlr_pago;
 		$dataPagamento = $registro->data_ocorrencia;
 		$carteira      = $registro->carteira;
         $vlr_juros_multa = $registro->valor;
         $vlr_desconto = $registro->vlr_desconto;
-        echo $nossoNumero;
-        echo $vlr_desconto;
-        echo $dataPagamento;
-        echo $vlr_juros_multa;
-        var_dump($registro);
-		// você ja pode dar baixa
+		foreach ($registro->children as $child) {
+            if ($child->url_qrcode !== null) {
+                var_dump($child->url_qrcode);
+                var_dump($child->codigo_identificacao);
+            }
+        }
 	}
-    
 }
 ?>
