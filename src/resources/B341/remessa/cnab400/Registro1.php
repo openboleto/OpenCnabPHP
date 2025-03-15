aa5ea193b030a2c2402c3f587d1c585473621432
Breno Ryoti <brenopsilva95@hotmail.com>
2025-03-15 00:25:54 -0300

    correcao gerando remessa itau com multa

--- src/resources/B341/remessa/cnab400/Registro1.php
+++ src/resources/B341/remessa/cnab400/Registro1.php
@@ -250,16 +250,17 @@ class Registro1 extends Generico1
 
     public function __construct($data = null)
     {
-        if (empty($this->data))
+        if (empty($this->data)) {
             parent::__construct($data);
-        //$this->inserirMulta($data); nÃ£o consegui validar o arquivo enviando o registro 2, entao temporariamente ficara desativado
+            $this->inserirMulta($data);
+        }
     }
 
 
     public function inserirMulta($data)
     {
         if (isset($data['data_multa'])) {
-            $class = 'CnabPHP\resources\\' . RemessaAbstract::$banco . '\remessa\\' . RemessaAbstract::$layout . '\Registro2';
+            $class = 'CnabPHP\resources\\B' . RemessaAbstract::$banco . '\remessa\\' . RemessaAbstract::$layout . '\Registro2';
             $this->children[] = new $class($data);
         }
     }
